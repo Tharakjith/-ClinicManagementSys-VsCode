5881989b25159ed3ceb54314be4e65c5ce425593
ADMIN <ADMIN@DESKTOP-2LO49AN>
2025-01-08 20:51:14 +0530

    Your commit message

---
+++ src/app/shared/model/department.spec.ts
@@ -0,0 +1,10 @@
+import { Department } from './department';
+
+describe('Department', () => {
+  it('should create an instance', () => {
+    expect(new Department()).toBeTruthy();
+  });
+});
+
+
+
--- src/app/shared/model/user.ts
+++ src/app/shared/model/user.ts
@@ -1,2 +1,20 @@
+import { Role } from "./role";
+import { Staff } from "./staff";
+
 export class User {
+    
+RegistrationId: number = 0;
+  
+  StaffId: Staff = new Staff();
+  RoleId: number = 0;
+  Password: string = '';
+  Username: string = '';
+  RIsActive: boolean = false; 
+  RegisteredDate: Date = new Date(); 
+ 
+  
+  
+  
+ role:Role = new Role();
+
 }
