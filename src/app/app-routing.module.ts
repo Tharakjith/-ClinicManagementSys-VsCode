93a14d7fac4747a120d91fc2e644abb4ce800b3f
Tharakjith <tharakjithpradeep@gmail.com>
2025-01-08 22:29:05 +0530

    Your commit message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { MedicineManagementsComponent } from './medicine-managements/medicine-managements.component';
+
 
 const routes: Routes = [
 //Empty Routes
@@ -8,10 +10,18 @@ const routes: Routes = [
   path: '', redirectTo: 'auth/login', pathMatch: 'full'
 },
   //Lazy-loading
+  
+  {path : 'medicine-managements', component: MedicineManagementsComponent,
+    loadChildren: () => import ('./medicine-managements/medicine-managements.module')
+    .then(p => p.MedicineManagementsModule)
+  },
+  
   {path : 'auth', component: AuthComponent,
     loadChildren: () => import ('./auth/auth.module')
     .then(x => x.AuthModule)
   },
+
+
   //Wildcard routes
   {path: '**', redirectTo: 'auth/notfound', pathMatch:'full'}
 ];
