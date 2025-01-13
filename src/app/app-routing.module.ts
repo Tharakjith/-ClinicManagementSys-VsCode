7a0417aea9d9bdc46e4f738be5586b2e5a09d272
karthika-vijayan01 <karthu900@gmail.com>
2025-01-13 16:18:24 +0530

    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DoctorComponent } from './doctor/doctor.component';
 import { AuthComponent } from './auth/auth.component';
+import { AdminComponent } from './admin/admin.component';
+import { MedicineManagementsComponent } from './medicine-managements/medicine-managements.component';
+import { PatientsListComponent } from './receptionists/patients-list/patients-list.component';
+import { ReceptionistsComponent } from './receptionists/receptionists.component';
+import { PharmacistsComponent } from './pharmacists/pharmacists.component';
 
 const routes: Routes = [
 
@@ -12,18 +17,36 @@ const routes: Routes = [
   },
 
   //Lazy Loading
-  {path:'doctor',component:DoctorComponent,
+  {path:'doctor',
+    // component:DoctorComponent,
     loadChildren: () => import('./doctor/doctor.module')
-    .then(e=>e.DoctorModule)
+      .then(e => e.DoctorModule)
+  
   },
 
-//   {path:'auth',component:AuthComponent,
-//   loadChildren:() =>import('./auth/auth.module')
-//   .then(x => x.AuthModule)
-// },
+  {
+    path: 'patients', component: ReceptionistsComponent,
+    loadChildren: () => import('./receptionists/receptionists.module')
+      .then(x => x.ReceptionistsModule),
+  },
+  {
+    path: 'admin', component: AdminComponent,
+    loadChildren: () => import('./admin/admin.module').then(e => e.AdminModule)
+  },
 
+  {
+    path: 'medicine-managements', component: MedicineManagementsComponent,
+    loadChildren: () => import('./medicine-managements/medicine-managements.module')
+      .then(e => e.MedicineManagementsModule)
+  },
 
-  {path: '**',redirectTo: 'auth/notfound',pathMatch:'full'}
+  {
+    path: 'medicine-prescriptions', component: PharmacistsComponent,
+    loadChildren: () => import('./pharmacists/pharmacists.module')
+      .then(e => e.PharmacistsModule)
+  },
+
+  { path: '**', redirectTo: 'auth/notfound', pathMatch: 'full' }
 
 ];
 
